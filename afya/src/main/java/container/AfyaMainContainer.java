93d6e8749c8205c60c90f6b43da6ad2f1d500f8c
abhiagarwal53 <abhiagarwal53@gmail.com>
2015-11-15 01:49:20 -0500

    Add Appointments Rest Api as a Resource Config

--- afya/src/main/java/container/AfyaMainContainer.java
+++ afya/src/main/java/container/AfyaMainContainer.java
@@ -2,6 +2,7 @@ package container;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+import web.AppointmentsRestApi;
 import web.UserApi;
 
 public class AfyaMainContainer extends ResourceConfig {
@@ -9,6 +10,7 @@ public class AfyaMainContainer extends ResourceConfig {
 	public AfyaMainContainer() {
 		
 		register(UserApi.class);
+		register(AppointmentsRestApi.class);
 	}
 	
 
