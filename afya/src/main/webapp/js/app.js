7bc87bb3b36c114c00cf3f3424466e9a784ab27b
Melody <mjs585@cornell.edu>
2015-11-15 10:50:18 -0500

    Added patient to frontend Database

--- afya/src/main/webapp/js/app.js
+++ afya/src/main/webapp/js/app.js
@@ -6,6 +6,7 @@ var patientData = {
   'Joanne Kim': {age: 24, date: "2011/12/13"},
   'Abhishek Agarwal': {age: 19, date: "2014/12/13"},
   'Ghaitsa Kenang': {age: 15, date: "2011/12/13"},
+  'Sarah Schuyler': {age: 21, date: "2011/5/4"}
 };
 
 $.get("http://default-environment-yppgutk292.elasticbeanstalk.com/dossier/user/allUsers", function(data){
